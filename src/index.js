0cfa56d6f937c14d46af37689e800b4159688449
claudiaamprimo <claudia.amprimo@gmail.com>
2024-10-07 19:49:47 +0200

    rollback index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 import cors from 'cors';
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -14,37 +14,28 @@ import { testConnection, sequelize } from './db.js';
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { insertInitialUserData } from './start_data.js'; // Solo se usa para poblar inicialmente la DB
+import { insertInitialUserData } from './start_data.js'; // solo se usa para poblar inicialmente la DB
 
-// Configurar variables de entorno
 dotenv.config();
 
-// Configuración para obtener __dirname en ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Crear instancia de Express
 const app = express();
 
-const allowedOrigins = [
-  'http://localhost:4200',
-  'https://proyecto-final-rho-one.vercel.app'
-];
+sequelize.sync({ alter: true }).then(() => {
+  console.log('Modelos sincronizados con la base de datos');
+}).catch(error => {
+  console.error('Error al sincronizar los modelos con la base de datos:', error);
+});
 
+// Configura el middleware CORS para que peuda recibir solicitudes de POST, PUT, DELETE, UPDATE, etc.
 app.use(cors({
   credentials: true,
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      const msg = 'El CORS policy no permite el origen solicitado.';
-      return callback(new Error(msg), false);
-    }
-  }
+  origin: 'http://localhost:4200'
 }));
 
-// Middleware para analizar cookies
+//header and populate req.cookies with an object keyed by the cookie names
 app.use(cookieParser());
 
 // Middleware para analizar el cuerpo de las solicitudes con formato JSON
@@ -53,7 +44,9 @@ app.use(express.json());
 // Middleware para analizar el cuerpo de las solicitudes con datos de formulario
 app.use(express.urlencoded({ extended: true })); // Para analizar datos de formularios en el cuerpo de la solicitud
 
-// Servir archivos estáticos desde la carpeta 'uploads'
+await testConnection();
+// await insertInitialUserData(); // solo se usa para poblar la DB con start_data
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Configurar rutas
@@ -67,27 +60,6 @@ app.use('/mapbox', mapboxRoutes);
 app.use('/friends', amigoRoutes);
 app.use('/cost-distributions', costDistributionRoutes);
 
-// Función asíncrona para sincronizar la base de datos y iniciar el servidor
-const startServer = async () => {
-  try {
-    // Probar la conexión a la base de datos
-    await testConnection();
-
-    // Sincronizar los modelos con la base de datos
-    await sequelize.sync({ alter: true });
-    console.log('Modelos sincronizados con la base de datos');
-
-    // Insertar datos iniciales si es necesario (descomentar si es necesario)
-    // await insertInitialUserData();
-
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-      console.log(`Servidor iniciado en el puerto ${PORT}`);
-    });
-  } catch (error) {
-    console.error('Error al iniciar el servidor:', error);
-    process.exit(1);
-  }
-};
-
-startServer();
+app.listen(3000, () => {
+  console.log("Servidor iniciado en el puerto 3000");
+});
